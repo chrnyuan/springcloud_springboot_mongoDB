4dfc7ebb78c8f19e3173ddbaaf02ed4c800e0762
cuizhixiang <cuizhixiang_java@163.com>
2018-11-15 17:03:26 +0800

    cc

--- src/test/java/com/issmart/service/RecommendServiceTest.java
+++ src/test/java/com/issmart/service/RecommendServiceTest.java
@@ -7,8 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.issmart.Application;
-import com.issmart.entity.MemberFindLogEntity;
-import com.issmart.repository.MemberFindLogRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {Application.class})
@@ -17,29 +15,9 @@ public class RecommendServiceTest {
 	@Autowired
 	private RecommendService recommendService;
 	
-	@Autowired
-	private MemberFindLogRepository memberFindLogRepository;
-
-//	@Test
-//	public void testFirstStart() {
-//		fail("Not yet implemented");
-//	}
-//
 	@Test
 	public void testUpdateRecommendCollection() {
-		recommendService.updateRecommendCollection("F0F8F2DA97E3");
+		recommendService.updateRecommendCollection("920","F0F8F2DA97E3");
 	}
-//
-//	@Test
-//	public void testFindByBeaconMac() {
-//		fail("Not yet implemented");
-//	}
 
-	@Test
-	public void testInsertMemberFindLogEntity() {
-		MemberFindLogEntity memberFindLogEntity = new MemberFindLogEntity();
-		memberFindLogEntity.setBeaconMac("F0F8F2DA97E3");
-		memberFindLogEntity.setTimeStamp(System.currentTimeMillis() - 1000000);
-		memberFindLogRepository.insert(memberFindLogEntity);
-	}
 }
